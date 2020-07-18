4ca3559baf7820905bff6896a350519b5b63003e
ManiloxGT <60653867+ManelGonzalez-ops@users.noreply.github.com>
2020-07-18 17:47:58 +0300

    comented out stacticfiles_dir

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -149,9 +149,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 MEDIA_URL = "/kaka/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
